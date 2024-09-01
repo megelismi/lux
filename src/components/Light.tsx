8da016d9db9259e779d2948efca52b02726503e9
Megan Smith <meg.eli.smi@gmail.com>
2024-09-01 15:46:03 -0700

    allow components to set their own colors

--- src/components/Light.tsx
+++ src/components/Light.tsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import "./Light.css";
 
 function Light({ color }: { color: string }) {
+  const [hovering, setHovering] = useState(true);
+
   return (
     <>
-      <div className="light-container">
-        <div className={`${color} light-body`}></div>
+      <div
+        className="light-container"
+        onMouseEnter={() => setHovering(true)}
+        onMouseLeave={() => setHovering(false)}
+      >
+        <div
+          className="light-body"
+          style={
+            hovering
+              ? { backgroundColor: `${color}` }
+              : { backgroundColor: "transparent" }
+          }
+        ></div>
       </div>
     </>
   );
--- src/components/LightGrid.tsx
+++ src/components/LightGrid.tsx
@@ -3,35 +3,48 @@ import Light from "./Light";
 import "./Light.css";
 
 function LightGrid() {
-  const [color, setColor] = useState("green");
+  const [color, setColor] = useState("#80ffdb");
+
+  // color palette
+
+  // 80ffdb
+  // 72efdd
+  // 64dfdf
+  // 56cfe1
+  // 48bfe3
+  // 4ea8de
+  // 5390d9
+  // 5e60ce
+  // 6930c3
+  // 7400b8
 
   return (
     <>
       <div className="light-grid">
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
-        <Light color={color} />
+        <Light color={"#80ffdb"} />
+        <Light color={"#72efdd"} />
+        <Light color={"#64dfdf"} />
+        <Light color={"#56cfe1"} />
+        <Light color={"#48bfe3"} />
+        <Light color={"#4ea8de"} />
+        <Light color={"#5390d9"} />
+        <Light color={"#5e60ce"} />
+        <Light color={"#6930c3"} />
+        <Light color={"#7400b8"} />
+        <Light color={"#80ffdb"} />
+        <Light color={"#72efdd"} />
+        <Light color={"#64dfdf"} />
+        <Light color={"#56cfe1"} />
+        <Light color={"#48bfe3"} />
+        <Light color={"#4ea8de"} />
+        <Light color={"#5390d9"} />
+        <Light color={"#5e60ce"} />
+        <Light color={"#6930c3"} />
+        <Light color={"#7400b8"} />
+        <Light color={"#80ffdb"} />
+        <Light color={"#72efdd"} />
+        <Light color={"#64dfdf"} />
+        <Light color={"#56cfe1"} />
       </div>
     </>
   );
