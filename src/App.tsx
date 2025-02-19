0045e83bc9b66ad341c332eba1f56a1bbd7ff3fe
Megan Smith <meg.eli.smi@gmail.com>
2025-02-19 12:45:25 -0800

    move Layout up one level

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 // import Grid from "./components/Grid";
-import Layout from "./components/Layout";
+import Layout from "./Layout";
 // import ControlPanel from "./components/ControlPanel";
 
 function App() {
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,14 @@
 import "./Layout.css";
+import ControlPanel from "./components/ControlPanel";
+// import Grid from "./components/Grid";
 
-function Layout({}) {
+function Layout() {
   return (
     <div className="layout">
       <div className="header">HEADER</div>
-      <div className="sidebar">SIDEBAR</div>
+      <div className="sidebar">
+        <ControlPanel />
+      </div>
       <div className="main">MAIN</div>
     </div>
   );
